d94fbeccaed4c04b7843e03f8617fc8f959be11d
Alyson Takai <shtakai@gmail.com>
2016-06-22 13:29:01 -0700

    implement authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions' => 'sessions#index'
   post 'sessions' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
